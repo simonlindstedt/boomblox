6c198bfd1c0441577c5ad60f5efcc9eb08d4fb5c
simonlindstedt <simon.lindstedt@gmail.com>
2021-10-27 10:48:39 +0200

    added default volume to gain

--- src/Classes/GainBox/index.js
+++ src/Classes/GainBox/index.js
@@ -2,10 +2,11 @@ import Gain from "../Audio/Gain";
 import BasicBox from "../BasicBox";
 
 export default class GainBox extends BasicBox {
-  constructor(x, y, w, h) {
+  constructor(x, y, w, h, volume = 0.2) {
     super(x, y, w, h);
-    this.audioNode = new Gain();
     this.type = "gain";
+    this.audioNode = new Gain();
+    this.audioNode.setVolume(volume);
     this.init();
   }
 }
