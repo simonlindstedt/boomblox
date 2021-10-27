eb63973d8dfe17241c040da38a40e8ff50efd280
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-10-27 11:24:14 +0200

    integrate mic sound with audio context

--- src/Classes/RecordingBox/index.js
+++ src/Classes/RecordingBox/index.js
@@ -1,15 +1,14 @@
 import { Sprite } from '@pixi/sprite';
-import { Sound } from '@pixi/sound';
 import BasicBox from '../BasicBox';
 import recordButton from './images/record.png';
 import stopButton from './images/pause.png';
 import Visualizer from '../Visualizer';
-import Audio from '../Audio/Audio';
+import audio from '../Audio/Audio';
 
 export default class RecordingBox extends BasicBox {
   constructor(x, y, w, h) {
     super(x, y, w, h);
-    this.visualizer = new Visualizer(Audio.context);
+    this.visualizer = new Visualizer();
     this.graphics = {
       ...this.graphics,
       recordBtn: new Sprite.from(recordButton),
@@ -49,7 +48,7 @@ export default class RecordingBox extends BasicBox {
         const mediaRecorder = new MediaRecorder(stream);
 
         let chunks = [];
-        this.visualizer.createMediaStream(stream);
+        this.visualizer.createMediaStreamSourceAndConnectToAnalyser(stream);
 
         this.graphics.recordBtn.on('mousedown', (e) => {
           mediaRecorder.start();
@@ -71,14 +70,27 @@ export default class RecordingBox extends BasicBox {
           const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
           chunks = [];
           const audioURL = window.URL.createObjectURL(blob);
+
           console.log('recorder stopped');
 
-          const sound = Sound.from(audioURL);
-          sound.play({ loop: true });
+          playSound(audioURL);
         };
 
         mediaRecorder.ondataavailable = function (e) {
           chunks.push(e.data);
+        };
+
+        const playSound = async (audioURL) => {
+          const audioContext = audio.context;
+          const source = audioContext.createBufferSource();
+          const audioBuffer = await fetch(audioURL)
+            .then((res) => res.arrayBuffer())
+            .then((ArrayBuffer) => audioContext.decodeAudioData(ArrayBuffer));
+
+          source.buffer = audioBuffer;
+          source.connect(audioContext.destination);
+          source.start();
+          source.loop = true;
         };
       }, this.onError);
     } else {
