b195533de8347caf71abbbf56398516a1633fab0
simonlindstedt <simon.lindstedt@gmail.com>
2021-11-02 14:57:05 +0100

    added side-menu

---
+++ src/components/SideMenu/index.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { StyledSideMenu, StyledMenuButton } from "./styles";
+
+const SideMenu = ({ children }) => {
+  const [active, setActive] = useState(false);
+
+  const menuVariants = {
+    hidden: { x: "100%" },
+    visible: { x: 0 },
+  };
+
+  const buttonVariants = {
+    inactive: { x: 0 },
+    active: { x: -200 },
+  };
+  return (
+    <>
+      <StyledMenuButton
+        initial="inactive"
+        animate={active ? "active" : "inactive"}
+        variants={buttonVariants}
+        onClick={() => {
+          setActive(!active);
+        }}
+      >
+        ⬅️
+      </StyledMenuButton>
+      <StyledSideMenu
+        initial="hidden"
+        animate={active ? "visible" : "hidden"}
+        variants={menuVariants}
+      >
+        {children}
+      </StyledSideMenu>
+    </>
+  );
+};
+
+export default SideMenu;
