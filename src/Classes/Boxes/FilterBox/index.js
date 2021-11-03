f21311971c1c62c85cbc9026dce522aad0f68ed6
simonlindstedt <simon.lindstedt@gmail.com>
2021-11-03 15:22:49 +0100

    updates to filter box

--- src/Classes/Boxes/FilterBox/index.js
+++ src/Classes/Boxes/FilterBox/index.js
@@ -1,28 +1,24 @@
 import BasicBox from "../BasicBox";
 import Filter from "../../Audio/Filter";
+import Gain from "../../Audio/Gain";
 
 export default class FilterBox extends BasicBox {
-  constructor(x, y, w, h) {
-    super(x, y, w, h);
+  constructor(x, y, w, h, settings) {
+    super(x, y, w, h, settings);
     this.type = "filter";
     this.canConnect = ["master"];
-    this.audioNode = new Filter();
+    this.input = new Filter(settings.type, settings.freq);
+    this.output = new Gain();
     this.init();
   }
 
   connectTo(box) {
-    switch (box.type) {
-      case "master":
-        this.connections.push({ id: box.id, position: box.position });
-        this.audioNode.node.connect(box.audioNode.node);
-        break;
-      default:
-        return;
-    }
+    this.addToConnectionList(box);
+    this.output.connectTo(box.input);
   }
 
   disconnectFrom(box) {
     this.connections = this.connections.filter((item) => item.id !== box.id);
-    this.audioNode.node.disconnect(box.audioNode.node);
+    this.output.disconnectFrom(box.input);
   }
 }
