cd0b0704f08bcec57ea4575b53d0c0d4ed189afa
rikardseg <70521759+rikardseg@users.noreply.github.com>
2021-11-12 11:26:00 +0100

    clean

--- src/Classes/Boxes/DrumBox/index.js
+++ src/Classes/Boxes/DrumBox/index.js
@@ -7,7 +7,6 @@ import Bass1 from './sounds/bass_sample.mp3';
 import HiHat2 from './sounds/edm-hi-hat.wav';
 import Clap2 from './sounds/good-snare.wav';
 import Bass2 from './sounds/808-kick.wav';
-import Clock from '../../Clock';
 
 export default class DrumBox extends BasicBox {
   constructor(x, y, w, h, mediator, settings) {
@@ -18,7 +17,6 @@ export default class DrumBox extends BasicBox {
     this.audioNode;
     this.clapNode;
     this.bassNode;
-    this.clock = new Clock();
     this.currentTime;
 
     this.samples = [HiHat1, Clap1, Bass1, HiHat2, Clap2, Bass2];
@@ -27,37 +25,6 @@ export default class DrumBox extends BasicBox {
     this.handlePlay();
 
     this.init();
-    // this.clock.start();
-    // this.clock.onmessage = (e) => {
-    //   if (e.data === 'tick') {
-    //     console.log(e.data);
-    //     Object.keys(this.sequences).forEach((key) => {
-    //       let { step, id, sequence } = this.sequences[key];
-    //       step > sequence.length ? (step = 0) : step;
-
-    //       if (sequence[step]) {
-    //         this.playSound(id);
-    //       }
-    //       this.sequences[key].step++;
-    //     });
-    //   }
-    // };
-  }
-
-  handlePlay() {
-    window.addEventListener('keydown', (e) => {
-      switch (e.key) {
-        case 'a':
-          this.playSound(2);
-          break;
-        case 's':
-          this.playSound(4);
-          break;
-        case 'd':
-          this.playSound(3);
-          break;
-      }
-    });
   }
 
   connectTo(box) {
