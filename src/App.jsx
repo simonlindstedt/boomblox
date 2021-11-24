769380116a4f69257cfc3436ee36c3be94f21e4c
simonlindstedt <simon.lindstedt@gmail.com>
2021-11-24 11:55:26 +0100

    mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import './main.scss';
 
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Canvas from './components/Canvas';
 import StartPage from './components/StartPage';
 
 const App = () => {
-  return (
-    <main>
-      <StartPage />
-      <Canvas />
-    </main>
-  );
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    if (
+      navigator.userAgent.includes('Android') ||
+      navigator.userAgent.includes('iPhone') ||
+      navigator.userAgent.includes('iPad')
+    ) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, []);
+
+  if (!mobile) {
+    return (
+      <main>
+        <StartPage />
+        <Canvas />
+      </main>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <p>Not yet supported on mobile :(</p>
+      </div>
+    );
+  }
 };
 
 export default App;
